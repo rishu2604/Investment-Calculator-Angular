85eeb4312e8d86db9c6730276b52065500090fba
Rishu Anand <rishu.anand@knowmax.ai>
2025-01-02 00:44:48 +0530

    first commit

---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,20 @@
+import { CurrencyPipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [CurrencyPipe], // In-built pipe for transforming currency output
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css',
+})
+export class InvestmentResultsComponent {
+  @Input() result?: {
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[];
+}
